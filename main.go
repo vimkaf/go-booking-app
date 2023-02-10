a7b0f4a48eb2d2ffe01e373f3b6eed5a082cace9
Olukayode Adamolekun <vimkaf@gmail.com>
2023-02-10 18:57:52 +0100

    Added booking feature

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	conferenceName := "PHPLagos"
+	const totalTicketsAvailable uint = 100
+	var totalTicketsRemaining = 100
+
+	fmt.Println("Welcome to", conferenceName, "You are highly invited")
+	fmt.Printf("There are %v tickets reamining out of %v available tickets \n", totalTicketsRemaining, totalTicketsAvailable)
+
+	bookings := []string{} // var bookings [totalTicketsAvailable]string
+
+	for {
+		var firstName, lastName, email string
+		var userTicketPurchaseCount int = 0
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("How many tickets are you buying: ")
+		fmt.Scan(&userTicketPurchaseCount)
+
+		totalTicketsRemaining -= userTicketPurchaseCount
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Congrats!! %v %v, You have purchased %v tickets for %v. You will receive a confirmation at %v \n", firstName, lastName, userTicketPurchaseCount, conferenceName, email)
+		fmt.Printf("There are %v tickets left \n", totalTicketsRemaining)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+
+			firstNames = append(firstNames, names[0])
+
+		}
+
+		fmt.Printf("Bookings: %v \n", firstNames)
+
+		if totalTicketsRemaining == 0 {
+			fmt.Print("Tickets sold out, till next year")
+		}
+
+	}
+
+}
